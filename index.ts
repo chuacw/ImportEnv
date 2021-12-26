f7021f61b00382b78f4c3461b8d09eca118f7a63
chuacw <1757930+chuacw@users.noreply.github.com>
2021-12-26 22:31:30 +0800

    Initial check-in
    Example to show how to use dotenv.

---
+++ index.ts
@@ -0,0 +1,10 @@
+require('dotenv').config();
+
+const VAR1=process.env.VAR1;
+const Bool1=process.env.Bool1;
+const Bool2=process.env.Bool2;
+
+console.log(VAR1);
+console.log(Bool1);
+console.log(Bool2);
+
